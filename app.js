81598b3ec1d6deed83ddf6147afcfd33191378fe
Apon Palanuwech <palaa159@newschool.edu>
2013-11-14 07:14:01 -0500

    006

--- app.js
+++ app.js
@@ -71,9 +71,11 @@ pcap_session.on('packet', function(raw_packet) {
 		// data_byte = packet.link.ip.tcp.data_bytes,
 		data = packet.link.ip.tcp.data;
 
-		if(dst_port == 9001) {
-			console.log(dst_ip, dst_port, data.toString());
-		}
+		console.log(packet);
+
+		// if(dst_port == 9001) {
+		// 	console.log(dst_ip, dst_port);
+		// }
 });
 
 // exec airodump with 5 second interval
